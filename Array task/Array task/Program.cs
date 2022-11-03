f15f37607c78fe9d6b77a9593565e78514dfde72
AliAkbarov99 <tu6lw8g66@code.edu.az>
2022-11-03 11:56:23 +0400

    Array task is created

---
+++ Array task/Array task/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int[] arr = { 1, 2, 3, 4, };
+
+for (int i = 0; i < arr.Length; i++) 
+{
+    Console.WriteLine(arr[i]);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i] + 1);
+}
